73f4884a3794c5062986faac3b0612b483fa2531
0639Quercus <thsomsits.brie@gmail.com>
2022-09-15 14:03:50 +0700

    Complete Quiz 33

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,41 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.imgSrc}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {props.userName}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{props.status}</span>
+          {props.likesCount > 0 && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{props.likesCount} คน</span>
+            </div>
+          )}
+        </div>
+      </div>
 
-      {/*render Reply here... */}
+      {/* Replies */}
+      {props.replies.map((r) => (
+        <Reply
+          imgSrc={r.userImagePath}
+          userName={r.username}
+          status={r.replyText}
+          likesCount={r.likeNum}
+        />
+      ))}
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import Comment from "./Comment";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src={props.imgSrc}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5 text-white">{props.userName}</span>
+      </div>
+      <span className="text-white">{props.status}</span>
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span style={{ color: "#B0B3B8" }}>{props.likesCount} คน</span>
+      </div>
+      <hr className="m-0 border" />
+
+      {/* Comments, just in case */}
+      {props.comments.map((c) => (
+        <Comment
+          imgSrc={c.userImagePath}
+          userName={c.username}
+          status={c.replyText}
+          likesCount={c.likeNum}
+          replies={c.replies}
+        />
+      ))}
+    </div>
+  );
 }
